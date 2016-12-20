ebe71867301551dadf46f436b347a02b249c1e55
Tom Melluish <Tom.Melluishcartesian.com>
2016-12-20 15:58:41 +0000

    Using zoo to get to a plottable time series

--- R/R-time-series-experiments.R
+++ R/R-time-series-experiments.R
@@ -1,10 +1,12 @@
 install.packages("data.table")
 install.packages("TTR")
 install.packages("lubridate")
+install.packages("zoo")
 
 require("data.table")
 require("TTR")
 require("lubridate")
+require("zoo")
 
 setwd("c:/users/tom.melluish/src/time-series-experiments")
 
@@ -14,6 +16,9 @@ sample_data <-fread("data/sample-data.csv")
 posixct_dates <- as.POSIXct(sample_data$Date,format="%Y-%m-%dT%H:%M:%S")
 
 lubridate_dates <- ymd_hms(sample_data$Date)
+
+metric.ts <- zoo(x=sample_data$Metric,order.by=ymd_hms(sample_data$Date))
+plot(metric.ts)
 
 
 #metric.ts <- ts(sample_data$Metric,frequency=8760,start=)
