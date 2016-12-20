2c5d82f30274d8c32f9c857b5af60ee625473e3e
Tom Melluish <Tom.Melluishcartesian.com>
2016-12-20 15:39:06 +0000

    Lubridate is much better for datetime parsing

--- R/R-time-series-experiments.R
+++ R/R-time-series-experiments.R
@@ -1,8 +1,10 @@
 install.packages("data.table")
 install.packages("TTR")
+install.packages("lubridate")
 
 require("data.table")
 require("TTR")
+require("lubridate")
 
 setwd("c:/users/tom.melluish/src/time-series-experiments")
 
@@ -10,6 +12,8 @@ setwd("c:/users/tom.melluish/src/time-series-experiments")
 sample_data <-fread("data/sample-data.csv")
 
 posixct_dates <- as.POSIXct(sample_data$Date,format="%Y-%m-%dT%H:%M:%S")
+
+lubridate_dates <- ymd_hms(sample_data$Date)
 
 
 #metric.ts <- ts(sample_data$Metric,frequency=8760,start=)
